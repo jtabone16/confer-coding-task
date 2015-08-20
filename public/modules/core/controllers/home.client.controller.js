3b0eade2ba5686f9bcf58d58146cb09c08895a97
John Tabone <jtabone16@gmail.com>
2015-08-20 10:37:26 -0400

    Updated README.md and got rid of some unnecessary scaffolding files generated by yeoman

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -17,7 +17,10 @@ angular.module('core').controller('HomeController', ['$scope', '$http', 'Authent
 		};
 
 		//Bool to notify user of error
-		$scope.error_dialog = {'show': false};
+		$scope.error_dialog = {
+			'show': false,
+			'message': ''
+		};
 
 		//Array to handle multiple books
 		$scope.books = [];
@@ -33,6 +36,13 @@ angular.module('core').controller('HomeController', ['$scope', '$http', 'Authent
 
 		$scope.getSearchResults = function (keyEvent){
 			if (keyEvent.which === 13){
+
+					//Explicit error handling for lack of ISBN-10 numbers...saves time making unneeded requests
+					if ($scope.search_terms.length < 10){
+						$scope.error_dialog.show = true;
+						$scope.error_dialog.message = 'Please enter a valid 10 or 13 digit ISBN number ';
+						return;
+					}
 
 					$scope.books = [];
 					var isbn_list = $scope.search_terms.split(',');
@@ -55,8 +65,10 @@ angular.module('core').controller('HomeController', ['$scope', '$http', 'Authent
 									$scope.book = [];
 								}
 
+								//If no books are found with the provided ISBN-10 numbers...
 								if ($scope.books.length === 0){
 									$scope.error_dialog.show = true;
+									$scope.error_dialog.message = 'Error requesting books from server. Please try again and make sure multiple ISBNs are comma-separated.';
 								}
 								else{
 									$scope.error_dialog.show = false;
